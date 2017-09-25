75177a4e25bfa6b94dd84ac8e28cfe55fb551840
zhc <zhcmoe@163.com>
2017-09-25 17:18:54 +0800

    实现新闻详情页面

--- src/component/news/newsinfo.vue
+++ src/component/news/newsinfo.vue
@@ -1,22 +1,56 @@
 <template>
 	<div class="temp">
-		资讯详情{{id}}
+		<div class="title">
+			<h4>{{info.title}}</h4>
+			<p>{{info.add_time | datefmt('YYYY-MM-DD HH:mm:ss')}}，阅读：{{info.click}}次</p>
+		</div>
+		<div id="content" v-html='info.content'>
+			
+		</div>
 	</div>
 </template>
 
 <script>
+	import { Toast } from 'mint-ui';
+	import { Indicator } from 'mint-ui';
 	export default{
 		data(){
 			return {
-				id:0
+				id:0,
+				info:[]
 			}
 		},
 		created(){
+			Indicator.open('加载中...');
 			this.id = this.$route.params.id
+			this.getinfo();
+		},
+		methods:{
+			getinfo(){
+				var url = 'http://182.254.146.100:8899/api/getnew/'+this.id;
+				this.$http.get(url).then(function(res){
+					var data = res.body;
+					Indicator.close()
+					if (data.status !=0) {
+						Toast(data.message);
+						return
+					}
+					this.info = data.message[0];
+				})
+			}
 		}
 	}
 </script>
 
 <style scoped>
-	
+	.title h4{
+		color: #0F73EF;
+	}
+	.title p{
+        color:rgba(0,0,0,0.5);
+    }
+
+    .title,.content{
+        padding: 5px;
+    }
 </style>
