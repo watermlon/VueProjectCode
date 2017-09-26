56c772ca0ef4179169033b209d92e33d146ed53f
zhc <zhcmoe@163.com>
2017-09-26 15:40:04 +0800

    图片分享列表完成

--- src/component/photo/photolist.vue
+++ src/component/photo/photolist.vue
@@ -1,31 +1,72 @@
 <template>
 	<div class="temp">
 		<div id="cate">
-			<ul>
-				<li>全部</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
-				<li>分类</li>
+			<ul v-bind="{style:'width:'+ulWidth+'px'}">
+				<li @click='getimgdata(0)'>全部</li>
+				<li v-for='item in cates' :key='item.id' @click='getimgdata(item.id)'>{{item.title}}</li>
+			</ul>
+		</div>
+		<div>
+			<ul id="imglist">
+				<li v-for='item in imgdata'>
+					<img v-lazy="item.img_url">
+					<div id="desc">
+						<h5 v-text="item.title"></h5>
+						<p v-text="item.zhaiyao"></p>
+					</div>
+				</li>
 			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
-import apiline from '../subcom/comment.vue';
+import apiline from '../../kits/common.js';
+import { Toast } from 'mint-ui';
 	export default{
-
+		data(){
+			return{
+				cates:[],
+				ulWidth:320,
+				imgdata:[]
+			}
+		},
+		created(){
+			this.getcates();
+			this.getimgdata(0);
+		},
+		methods:{
+			getcates(){
+				var url = apiline.apiline+'/api/getimgcategory'
+				this.$http.get(url).then(function(res){
+					var data = res.body
+					if (data.status!=0) {
+						Toast(data.message);
+						return
+					}
+					this.cates=data.message;
+					var w = 62;
+					var count = data.message.length+1;
+					this.ulWidth=w*count;
+				})
+			},
+			getimgdata(cateid){
+				cateid=cateid||0;
+				var url = apiline.apiline+'/api/getimages/'+cateid;
+				this.$http.get(url).then(function(res){
+					var data = res.body
+					if (data.status!=0) {
+						Toast(data.message);
+						return
+					}
+					if (data.message.length==0) {
+						Toast('无数据')
+						return
+					}
+					this.imgdata=data.message;
+				})
+			}
+		}
 	}
 </script>
 
@@ -36,7 +77,6 @@ import apiline from '../subcom/comment.vue';
 		overflow-x: auto;
 	}
 	#cate ul{
-		width: 1000px;
 		margin: 0px;
 		padding-left: 10px;
 	}
@@ -46,5 +86,41 @@ import apiline from '../subcom/comment.vue';
 		color:#0094ff;
 		font-size: 14px;
 		padding-left: 6px;
+	}
+	#imglist{
+
+	}
+	#imglist ul {
+		padding-left: 0px;
+	}
+	#imglist li{
+		list-style:none;
+		position: relative ;
+	}
+	#imglist img{
+		width:100%;
+		height: 300px;
+	}
+
+	#desc{
+		width: 100%;
+		background-color: rgba(0,0,0,0.3);
+		position: absolute;
+		bottom: 2px;
+		left: 0px;
+	}
+
+	#desc h5{
+		color: #ffffff;
+		font-weight: bold;
+	}
+	#desc p{
+		color:#fff;
+	}
+
+	image[lazy=loading] {
+		width: 40px;
+		height: 300px;
+		margin: auto;
 	}
 </style>
